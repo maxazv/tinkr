bbb908ed7b02446f18b6193a92216607fddfd62f
maxazv <azevedomaximilian@gmail.com>
2021-08-21 01:16:04 +0200

    First Commit

---
+++ main.py
@@ -0,0 +1,223 @@
+import numpy as np
+import math
+from phiai import PhiAI
+from phiai import Layer
+# from nn import NeuralNet
+
+"""
+nn = NeuralNet([2, 2, 1])
+phiai = PhiAI([1, 2, 1])
+
+def tomfoolery(M):
+    for i in range(len(M)-1):
+        curr = M[0][i]
+        M[0][i] = -1
+        j = i
+        k = 0
+        while curr != -1:
+            temp = curr
+            l = j
+            j = len(M)-1 - k
+            k = l
+            curr = M[k][j]
+            M[k][j] = temp
+    return M
+
+def test_neuralnet():
+    data_arr = [[[1, 1], 0], [[0, 1], 1], [[1, 0], 1], [[0, 0], 0]]
+
+    # first guess
+    nn.guess(data_arr[0][0])
+    err = np.array([data_arr[0][1]]).T
+    print("First Guess Data:", data_arr[0][0])
+    print("First Guess Output:", nn.layers[nn.size-1])
+    print("Target Output:", data_arr[0][1])
+    print("First Guess Error:", np.sum(nn.loss_func(err)))
+    guess_e1 = np.sum(nn.loss_func(err))
+
+    nn.backprop(data_arr[0][1])
+    nn.calc_stepsize()
+    nn.guess(data_arr[0][0])
+    print("Error:", np.sum(nn.loss_func(err)))
+    guess_e2 = np.sum(nn.loss_func(err))
+    if guess_e2 < guess_e1:
+        print("Succeeced")
+    else:
+        print("Failed")
+
+    # [Learn]
+    #'''
+    for i in range(1):
+        for j in range(len(data_arr)):
+            nn.guess(data_arr[j][0])
+            nn.backprop(data_arr[j][1])
+            nn.calc_stepsize()
+        random.shuffle(data_arr)
+        
+    # guess after learning
+    nn.guess(data_arr[0][0])
+    exp = np.array([data_arr[0][1]]).T
+    print("\nData:", data_arr[0][0])
+    print("Output:", nn.layers[nn.size - 1])
+    print("Target Output:", data_arr[0][1])
+    err = np.sum(nn.loss_func(exp))
+    print("Error:", err, "\n")
+    if err < 0.1:
+        for i in range(len(data_arr)):
+            print(data_arr[i][1], "- NN Prediction:", nn.guess(data_arr[i][0]))
+    #'''
+    # [Show NN Contents]
+    '''
+    print("\nLayers:", nn.layers)
+    print("Weights:", nn.weights)
+    print("Biases:", nn.biases)
+    #print("Delta Weights", nn.d_weights)
+    #print("Delta Biases", nn.d_biases)
+    #print("Activation X", nn.activations)
+    '''
+"""
+
+
+def delta_log(x):
+    return math.e ** x / (1 + math.e ** x)
+
+
+""" -***-[ Neural Network Test ]-***- """
+def test_ai(ptest, verbose=0, testcase=0):
+    cl_test = ptest
+
+    # phiAI Class
+    t_data = [[0, 0],
+              [0.5, 1],
+              [1, 0]]
+    test_i = testcase
+
+    nn = PhiAI([1, 2, 1])
+    nnv2 = PhiAI([1, 2, 2, 1])
+
+    # Layer Class
+    X = [0, 0.5, 1]
+    Y_H = [0, 1, 0]
+    k = testcase
+
+    h = Layer(1, 2)
+    y = Layer(2, 1)
+    lr = 0.075
+
+    if cl_test == 0:
+        # --[ Feedforward Algorithm ]--
+
+        h.ff(np.array([X[k]]))
+        y.ff(h.output, False)
+        l_v1 = y.output
+
+        # --[ Loss Func ]--
+
+        sr = (Y_H[k] - y.output) ** 2
+        l_v2 = sr
+
+        # --[ Update Params via Gradients ]--
+
+        # --gradient output bias
+        delta_sr = -2 * (Y_H[k] - y.output)
+        delta_y_b = delta_sr
+        # --update params
+        y.b -= delta_y_b * lr
+        # --more gradients and updates for y
+        delta_y_w = delta_sr * h.output.T
+        y.w -= delta_y_w * lr
+
+        # ++gradients and updates for h
+        delta_h_b = delta_sr * y.w * delta_log(h.z.T)
+        delta_h_w = delta_sr * y.w * delta_log(h.z.T) * np.array([X[k]])
+        h.b -= delta_h_b.T * lr
+        h.w -= delta_h_w.T * lr
+        h.ff(np.array([X[k]]))
+        y.ff(h.output, False)
+        l_v3 = y.output
+        l_v4 = (Y_H[k] - y.output) ** 2
+
+        if verbose > 0:
+            print(f'First Guess: {l_v1}')
+            print(f'Expected Output: [{Y_H[k]}]')
+            print(f'     -> Error: {l_v2}')
+            print('--------------------------------')
+            print(f'Adjusted Guess: {l_v3}')
+            print(f'     -> Error: {l_v4}\n')
+
+    elif cl_test == 1:
+        """ -***-[ Neural Network Class ]-***- """
+        # Test PhiAI Class
+        # + first prediction
+        nn.predict(np.array([t_data[test_i][0]]))
+        n_v1 = nn.layers[nn.size - 1].output
+        n_v2 = nn.loss(t_data[test_i][1])
+        # + adjust params once
+        nn.adjust(t_data[test_i][1])
+        nn.predict(np.array([t_data[test_i][0]]))
+        n_v3 = nn.layers[nn.size - 1].output
+        n_v4 = nn.loss(t_data[test_i][1])
+
+        if verbose > 0:
+            print(f'First Guess: {n_v1}')
+            print(f'Expected Output: {t_data[test_i][1]}')
+            print(f'     -> Error: {n_v2}')
+            print('--------------------------------')
+            print(f'Adjusted Guess: {n_v3}')
+            print(f'     -> Error: {n_v4}')
+
+        if verbose > 1:
+            nn.train(t_data)
+            nn.predict(np.array([t_data[0][0]]))
+            print('--------------------------------')
+            print("\nExpected:", t_data[0][1])
+            print("Predicted:", nn.loss(t_data[0][1]))
+
+            nn.predict(np.array([t_data[1][0]]))
+            print("\nExpected:", t_data[1][1])
+            print("Predicted:", nn.loss(t_data[1][1]))
+
+            nn.predict(np.array([t_data[2][0]]))
+            print("\nExpected:", t_data[2][1])
+            print("Predicted:", nn.loss(t_data[2][1]))
+
+    elif cl_test == 2:
+        nnv2.predict(np.array([t_data[test_i][0]]))
+        print(f'First Guess: {nnv2.layers[nnv2.size-1].output}')
+        print(f'Expected: {t_data[test_i][1]}')
+        print(f'First Guess Error: {nnv2.loss(t_data[test_i][1])}')
+        nnv2.adjust(t_data[test_i][1])
+        nnv2.predict(np.array([t_data[test_i][0]]))
+        print('--------------------------------')
+        print(f'Adjusted Guess: {nnv2.layers[nnv2.size - 1].output}')
+        print(f'Expected: {t_data[test_i][1]}')
+        print(f'Adjusted Guess Error: {nnv2.loss(t_data[test_i][1])}')
+
+        if verbose > 1:
+            nnv2.train(t_data)
+            nnv2.predict(np.array([t_data[0][0]]))
+            print('--------------------------------')
+            print("\nExpected:", t_data[0][1])
+            print("Predicted:", nnv2.loss(t_data[0][1]))
+
+            nnv2.predict(np.array([t_data[1][0]]))
+            print("\nExpected:", t_data[1][1])
+            print("Predicted:", nnv2.loss(t_data[1][1]))
+
+            nnv2.predict(np.array([t_data[2][0]]))
+            print("\nExpected:", t_data[2][1])
+            print("Predicted:", nnv2.loss(t_data[2][1]))
+
+
+if __name__ == '__main__':
+    '''
+    m = [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9]]
+    m = tomfoolery(m)
+    print(m)
+    '''
+    test = 2
+    verbose_level = 1
+    test_case = 0
+    test_ai(test, verbose_level, test_case)
---
+++ phiai.py
@@ -0,0 +1,141 @@
+import numpy as np
+import math
+
+
+class Layer:
+    def __init__(self, data_n, output_n):
+        """
+        Utilised by the PhiAI a single Layer which consists of Weights, Biases, "activated"-Value and an
+        Output based on the previous named Parameters
+        :param data_n: Amount of Neurons of (the previous) Layer
+        :param output_n: Amount of Neurons of the next Layer
+        """
+        self.w = np.random.normal(0, 1, size=(data_n, output_n))
+        self.b = np.zeros((1, output_n))
+        self.z = 0
+        self.output = 0
+        self.form = (data_n, output_n)
+
+    def ff(self, data, activation=True):
+        """
+        Calculates the output of a single Layer with the according weights, biases and provided data
+        :param data: The Contents/ Output of the previous Layer
+        :param activation: Whether the Output should be transformed via an Activation-Function
+        :return: Output of the Layer in numpy.arr-format
+        """
+        a = np.matmul(data, self.w) + self.b
+        self.z = a
+        if activation:
+            self.output = np.log(1 + math.e ** a)
+            return self.output
+        self.output = a
+        return self.output
+
+
+class PhiAI:
+    def __init__(self, layer_shapes):
+        """
+        Neural Network Class - made by Max
+        :param layer_shapes: Structure of the Neural Network in List-Format
+        """
+        self.size = len(layer_shapes) - 1
+        self.last_input = -1
+        self.layers = [Layer(layer_shapes[i], layer_shapes[i + 1]) for i in range(self.size)]
+        self.output = self.layers[self.size-1].output
+
+        self.lr = 0.05
+
+    @staticmethod
+    def __activation(x, method='log', prime=True):
+        if prime:
+            if method == 'log':
+                return math.e ** x / (1 + math.e ** x)
+            elif method == 'sigmoid':
+                return x * (1 - x)
+        return 0
+
+    def loss(self, target):
+        return (target - self.layers[self.size - 1].output) ** 2
+
+    def predict(self, data):
+        """
+        Calculates output based on weights and biases and stores result in self.layers (feedforward)
+        :param data: Data to be processed by Phi-Neural-Network
+        :return: Returns the calculated output
+        """
+        if data.shape[0] != self.layers[0].form[0]:
+            print('[ERR]: Dimensions do not match')
+            return -1
+
+        self.last_input = data
+        curr_output = data
+        for i in range(len(self.layers)):
+            if i == len(self.layers) - 1:
+                return self.layers[i].ff(curr_output, False)
+
+            curr_output = self.layers[i].ff(curr_output)
+        self.output = self.layers[self.size - 1].output
+        return curr_output
+
+    """ TO BE OPTIMISED HEAVILY -> PROBLEM: Transpose/ Shapes of Matrices
+     TEST WITH ORIGINAL PROVED METHOD 
+     IMPLEMENT BATCH-GRADIENT-DESCENT """
+    def adjust(self, target):
+        """
+        Adjusts Weights and Biases based on the calculated Gradients
+        :param target: The expected Output for the last used Inputs
+        """
+        delta = -2 * (target - self.layers[self.size - 1].output)  # mult by d_activation if necessary
+        for i in range(self.size - 1, 0, -1):
+            if i == self.size - 1:
+                self.layers[i].b -= delta * self.lr
+            else:
+                self.layers[i].b -= delta.T * self.lr
+
+            self.layers[i].w -= delta * self.layers[i - 1].output.T * self.lr
+            delta = np.matmul(self.layers[i].w, delta) * self.__activation(self.layers[i - 1].z.T, 'log', True)
+            # original: delta = delta * self.layers[i].w * self.__activation(self.layers[i - 1].z.T, 'log', True)
+
+        self.layers[0].b -= delta.T * self.lr
+        self.layers[0].w -= (delta * self.last_input).T * self.lr
+
+        return True
+
+    def train(self, training, max_epochs=250, lowest_err=0.01):
+        """
+        Utilises self.predict as well as self.adjust to optimise the Neural Network to the given data
+        :param training: The training data in List-Format: [[Input, Expected], ...]
+        :param max_epochs: The maximum training iterations before the Neural Network stops its training
+        :param lowest_err: The lowest error value before the Neural Network stops its training
+        """
+        lowest_err = lowest_err
+        max_epochs = max_epochs
+        c, err, i = (0, ) * 3
+        while c < max_epochs:
+            self.adjust(training[i][1])
+            self.predict(np.array([training[i][0]]))
+            err += (training[i][0] - self.layers[self.size - 1].output) ** 2
+            if (c % len(training)) == 0:
+                i = 0
+                err = 0
+                if err < lowest_err:
+                    print('REACHED')
+                    return
+                np.random.shuffle(training)
+            c += 1
+
+    def load_model(self, layers):
+        if len(self.layers) != len(layers):
+            return False
+
+        for i in range(len(layers)):
+            shape = layers[i][0].shape
+            if shape != self.layers[i].form:
+                return False
+
+            curr = Layer(shape[0], shape[1])
+            curr.w = layers[i][0]
+            curr.b = layers[i][1]
+
+            self.layers[i] = curr
+        return True
