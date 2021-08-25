78b50e403a5be9e9da87b5e125df9d325c54cbd0
maxazv <azevedomaximilian@gmail.com>
2021-08-25 16:52:57 +0200

    last stable

--- src/tinkr/mnist_digit.py
+++ src/tinkr/mnist_digit.py
@@ -1,9 +1,11 @@
+import random
+
 from dense import Dense
 from activations import ReLU
 from loss import mse, mse_prime
 from data_config import DataConfig
 
-# import numpy as np
+import numpy as np
 
 
 def config_data(layer_shapes, batch_size):
@@ -23,7 +25,6 @@ def config_data(layer_shapes, batch_size):
 def train(epochs, mb, lr, tinkr, batch_size):
     for e in range(epochs):
         error = 0
-        b_idx = 0
         for batch in mb:
             # feedforward
             onehot_y = DataConfig.one_hot(batch[0])
@@ -31,30 +32,20 @@ def train(epochs, mb, lr, tinkr, batch_size):
             for layer in tinkr:
                 output = layer.fforward(output)
             error += mse(onehot_y, output)
-            """
-            print(np.around(output, 3))
-            print()
-            print(np.sum(output, 1, keepdims=True))
-            print()
-            print(onehot_y)
-            print(error)"""
 
             # backpropagation
-            local_gradient = mse_prime(onehot_y, output)
+            gradient = mse_prime(onehot_y, output)
             for layer in reversed(tinkr):
-                local_gradient = layer.bprop(local_gradient, lr, batch_size)
+                gradient = layer.bprop(gradient, lr, batch_size)
 
-            if b_idx % 100 == 0:
-                error /= batch_size
-                print(f"{e+1}/{epochs}, {b_idx+1}/{len(mb)}, error={error}")
-            b_idx += 1
-
+        error /= len(mb)
+        print(f"{e + 1}/{epochs}, error={error}")
+        random.shuffle(mb)
 def predict(X, tinkr):
     print()
     z = X
     for layer in tinkr:
         z = layer.fforward(z)
-    # print(X.tolist(), "->", z)
 
 def main():
     # data/ hyperparameter configuration
@@ -64,8 +55,8 @@ def main():
     # print("Plotted: ", minibatches[0][0][9])
     # print("Plotted Onehot: \n", DataConfig.one_hot(minibatches[0][0]))
     # print(minibatches[0][1].shape)
-    epochs = 100
-    lr = 0.01
+    epochs = 75
+    lr = 0.25
 
     train(epochs, minibatches, lr, tinkr, 10)
 
